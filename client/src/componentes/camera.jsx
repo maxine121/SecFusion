4ab2096e9a489d51698bcdae139e24163712d1cd
Caíque Machado <caiquemsdo@gmail.com>
2026-05-28 22:49:55 -0300

    feat component folder

---
+++ client/src/componentes/camera.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+
+function Camera() {
+
+  return (
+    <>
+        <div class="w-96 h-80">w-96
+
+
+        </div>
+    </>
+  )
+}
+
+export default App
