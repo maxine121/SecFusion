2e496a4230af9d11bc681b9c43394f926ccc719e
clarinha <158226328+claramtenorio@users.noreply.github.com>
2026-06-17 15:39:26 -0300

    Add Usuarios component for user management

---
+++ client/src/paginas/Usuarios.jsx
@@ -0,0 +1,13 @@
+import { Users } from 'lucide-react'
+import EstadoPagina from '../componentes/EstadoPagina'
+
+function Usuarios() {
+  return (
+    <EstadoPagina
+      icon={Users}
+      titulo="Gerenciamento de Usuários"
+    />
+  )
+}
+
+export default Usuarios
